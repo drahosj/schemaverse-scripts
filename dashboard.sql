2c9f097a4525aaffaffa41c3df43d35d73314a93
Jake Drahos <jdrahos@cray.com>
2016-06-20 14:46:16 -0500

    Added action display to mining ships

--- dashboard.sql
+++ dashboard.sql
@@ -3,7 +3,7 @@ SELECT id, balance, fuel_reserve FROM my_player;
 
 SELECT 
 planets.id, planets.name, planets.mine_limit, planets.conqueror_id, planets.location,
-my_ships.name, my_ships.current_fuel, my_ships.speed, my_ships.direction
+my_ships.name, my_ships.current_fuel, my_ships.speed, my_ships.direction, my_ships.action
 FROM my_ships
 LEFT JOIN planets ON my_ships.action_target_id=planets.id 
 WHERE my_ships.action='MINE'
